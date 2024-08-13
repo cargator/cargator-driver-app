d31d95834d71478a4046afe168a022c0c8a1fc6b
Deepak Asati <asatideepak601@gmail.com>
2024-08-13 19:45:20 +0530

    Background live location

--- src/post-login/PetPujaScreen.tsx
+++ src/post-login/PetPujaScreen.tsx
@@ -12,6 +12,7 @@ import {
   ImageBackground,
   Linking,
   PermissionsAndroid,
+  Platform,
   Pressable,
   StyleSheet,
   Text,
@@ -57,6 +58,7 @@ import SidebarIcon from '../svg/SidebarIcon';
 import Spinner from '../svg/spinner';
 import {getSocketInstance, socketDisconnect} from '../utils/socket';
 import OnlineOfflineSwitch from './OnlineOfflineSwitch';
+import ReactNativeForegroundService from '@supersami/rn-foreground-service';
 
 const OrderStatusEnum = {
   ORDER_ACCEPTED: 'ACCEPTED', //(Order Created Successfully.)
@@ -571,6 +573,85 @@ const PetPujaScreen = ({navigation, route}: any) => {
     setLoading(false);
   };
 
+  const requestLocationPermission = async () => {
+    // Geolocation.requestAuthorization('always');
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location Permission',
+          message: 'App needs access to your location.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Location permission granted');
+      } else {
+        console.log('Location permission denied');
+      }
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
+        {
+          title: 'Background Location Permission',
+          message:
+            'We need access to your location ' +
+            'so you can get live quality updates.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  const updateforeground = () => {
+    ReactNativeForegroundService.add_task(() => startTracking(), {
+      delay: 100,
+      onLoop: true,
+      taskId: 'taskid',
+      onError: e => console.log(`Error logging:`, e),
+    });
+  };
+  const Notification = () => {
+    ReactNativeForegroundService.start({
+      id: 1244,
+      title: 'Location Tracking',
+      message: 'Location Tracking',
+      icon: 'ic_launcher',
+      button: false,
+      button2: false,
+      // buttonText: "Button",
+      // button2Text: "Anther Button",
+      // buttonOnPress: "cray",
+      setOnlyAlertOnce: "true",
+      color: '#000000',
+    });
+    startTracking();
+  };
+
+  const startTracking = async () => {
+    //  let s= Geolocation.requestAuthorization('always');
+
+    Geolocation.watchPosition(
+      (position: any) => {
+        let coordinates: any = [];
+        coordinates[0] = position.coords.longitude;
+        coordinates[1] = position.coords.latitude;
+        console.warn(Platform.OS, 'App Position tracking', coordinates);
+        console.log('App Position tracking', coordinates);
+      },
+      (error: any) => {
+        console.log('maperror in getting location', error.code, error.message);
+      },
+
+      {enableHighAccuracy: false, distanceFilter: 0},
+    );
+  };
+
   useEffect(() => {
     if (orderStartedRef.current) {
       return;
@@ -620,6 +701,13 @@ const PetPujaScreen = ({navigation, route}: any) => {
       emitLiveLocation();
     }
   }, [isDriverOnline, orderStartedRef.current]);
+
+  useEffect(() => {
+    requestLocationPermission();
+    updateforeground();
+    Notification();
+    startTracking();
+  }, []);
 
   return (
     <>
